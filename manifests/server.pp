008f9181d18483cd6679b45b9ccc22e8ca8670a3
Tommy Speigner <tspeigner@gmail.com>
2017-08-30 20:56:40 -0700

    adding logic to install with chocolatey if installed

--- manifests/server.pp
+++ manifests/server.pp
@@ -6,22 +6,35 @@ class win_mysql::server (
     $bind_address           = '127.0.0.1' ,
     $server_package_version = '5.7.18',
 ) {
-  # Install packages based on architecture x86 or win32
-  case $::architecture {
-    'x64': {
-        package { 'mysql':
-            ensure   => $server_package_version,
-            #source => "https://dev.mysql.com/get/Downloads/MySQL-${major_version}/mysql-${server_package_version}-winx86.zip"
-            provider => chocolatey,
+    # Install MySQL with Chocolatey, if it is installed
+    case $chocolateyversion {
+        '0': {
+            notify { 'chocolatey':
+              message => 'Chocolatey is not installed'
+            }
+        }
+        default: {
+            package { 'mysql':
+                ensure => installed,
+            }
         }
     }
-    default: {
-        package { 'mysql':
-            ensure => installed,
-            #source => "https://dev.mysql.com/get/Downloads/MySQL-${major_version}/mysql-${server_package_version}-win32.zip"
+#  # Install packages based on architecture x86 or win32
+#  case $::architecture {
+#    'x64': {
+#        package { 'mysql':
+#            ensure   => $server_package_version,
+#            #source => "https://dev.mysql.com/get/Downloads/MySQL-${major_version}/mysql-${server_package_version}-winx86.zip"
+#            provider => chocolatey,
+#        }
+#    }
+#    default: {
+#        package { 'mysql':
+#            ensure => installed,
+#            #source => "https://dev.mysql.com/get/Downloads/MySQL-${major_version}/mysql-${server_package_version}-win32.zip"
+#
+#        }
+#    }
 
-        }
-    }
-
-  }
+#  }
 }
