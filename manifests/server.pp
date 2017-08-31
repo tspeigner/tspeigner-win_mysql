2497c83c7467fee005bbd1ce7b2ec7463c58e585
Tommy Speigner <tspeigner@gmail.com>
2017-08-31 14:13:48 -0700

    again simplify

--- manifests/server.pp
+++ manifests/server.pp
@@ -6,7 +6,9 @@ class win_mysql::server (
 ) {
   # Install MySQL server based on version
   package { 'mysql':
-      ensure   => $server_package_version,
+      #ensure   => $server_package_version,
+      ensure   => installed,
+      #ensure   => absent,
       provider => chocolatey,
   }
 }
