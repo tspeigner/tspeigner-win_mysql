0f4b95c2575d88c3694e6c69f7e50149706f71e3
Tommy Speigner <tspeigner@gmail.com>
2017-08-31 15:41:37 -0700

    updated other text to reflect 2013

--- manifests/server.pp
+++ manifests/server.pp
@@ -2,15 +2,15 @@
 # win_mysql::server
 #
 class win_mysql::server {
-  # Install required package Microsoft Visual C++ 2015 Redistributable 
-  # https://www.microsoft.com/en-us/download/details.aspx?id=48145 
+  # Install required package Microsoft Visual C++ 2013 Redistributable 
+  # https://www.microsoft.com/en-us/download/details.aspx?id=40784 
   package { 'vcredist2013':
       ensure   => installed,
       provider => chocolatey,
       before   => Package['mysql'],
       notify   => Reboot['after_vcredist2013'],
   }
-  # Reboot the server after installing Microsoft Visual C++ 2015 Redistributable
+  # Reboot the server after installing Microsoft Visual C++ 2013 Redistributable
   # Reboot module required - https://forge.puppet.com/puppetlabs/reboot
   reboot { 'after_vcredist2013':
     apply => immediately,
