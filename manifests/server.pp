681c58689cad058511b5760c7a1b55b33b993d6f
Tommy Speigner <tspeigner@gmail.com>
2017-08-31 15:12:24 -0700

    added reboot after package is installed

--- manifests/server.pp
+++ manifests/server.pp
@@ -6,8 +6,14 @@ class win_mysql::server {
   package { 'vcredist2015':
       ensure   => installed,
       provider => chocolatey,
-      before   => Package['mysql']
+      before   => Package['mysql'],
+      notify   => Reboot['after_vcredist2015'],
   }
+  # Reboot the server after installing Microsoft Visual C++ 2015 Redistributable
+  reboot { 'after_vcredist2015':
+    apply => immediately,
+  }
+
   package { 'mysql':
       #ensure   => $server_package_version,
       ensure   => installed,
