49680e9f9f909e6bc43679786dc5fe0bc835546c
Tommy Speigner <tspeigner@gmail.com>
2017-09-01 14:00:00 -0700

    updating fact

--- lib/facter/mysqld_version.rb
+++ lib/facter/mysqld_version.rb
@@ -1,6 +1,6 @@
 Facter.add('mysqld_version') do
   setcode do
     mysqld_ver = Facter::Util::Resolution.exec('mysqld -V')
-    mysqld_ver.match(%r{\d+\.\d+\.\d+})[0] if mysql_ver
+    mysqld_ver.match(%r{\d+\.\d+\.\d+})[0] if mysqld_ver
   end
 end
