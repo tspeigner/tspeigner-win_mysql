cc2fec4eb7cc77b7d0bceba811c1f86800a8f135
Tommy Speigner <tspeigner@gmail.com>
2017-09-01 11:17:29 -0700

    trying the params again

--- manifests/server.pp
+++ manifests/server.pp
@@ -1,7 +1,9 @@
 # Classme
 # win_mysql::server
 #
-class win_mysql::server {
+class win_mysql::server (
+    $server_package_version,
+){
   # Install required package Microsoft Visual C++ 2013 Redistributable 
   # https://www.microsoft.com/en-us/download/details.aspx?id=40784 
   package { 'vcredist2013':
