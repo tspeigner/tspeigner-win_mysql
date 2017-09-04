aa887a98d8949d57b95a5f9b42200a00ce00e140
Tommy Speigner <tspeigner@gmail.com>
2017-09-04 13:42:30 -0700

    adding dollars to params

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,19 +4,19 @@
 
 class win_mysql::params {
 
-  bind_address           = '127.0.0.1'
-  default_engine         = 'UNSET'
-  purge_conf_dir         = false
-  max_connections        = 151
-  port                   = 3306
-  max_allowed_packet     = '16M'
-  ssl                    = false
-  client_package_ensure  = true
-  server_package_ensure  = true
-  server_package_version = '5.7.18'
-  choco_mysql_basedir    = 'c:\tools\mysql\current'
-  choco_mysql_bindir     = 'c:\tools\mysql\current\bin'
-  choco_config_file      = 'c:\tools\mysql\current\my.ini'
-  mysql_datadir          = 'C:\ProgramData\MySQL\data'
+  $bind_address           = '127.0.0.1'
+  $default_engine         = 'UNSET'
+  $purge_conf_dir         = false
+  $max_connections        = 151
+  $port                   = 3306
+  $max_allowed_packet     = '16M'
+  $ssl                    = false
+  $client_package_ensure  = true
+  $server_package_ensure  = true
+  $server_package_version = '5.7.18'
+  $choco_mysql_basedir    = 'c:\tools\mysql\current'
+  $choco_mysql_bindir     = 'c:\tools\mysql\current\bin'
+  $choco_config_file      = 'c:\tools\mysql\current\my.ini'
+  $mysql_datadir          = 'C:\ProgramData\MySQL\data'
 
 }
